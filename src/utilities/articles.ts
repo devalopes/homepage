f44180959ad67dce1a70959656299c39efa2f5bc
Alex Lopes <alexjonathanlopes@gmail.com>
2022-12-30 13:40:46 -0500

    Use authorization for GET requests

--- src/utilities/articles.ts
+++ src/utilities/articles.ts
@@ -1,11 +1,12 @@
-import Parser from 'rss-parser';
-import { Buffer } from 'buffer';
-import { storeWithExpiry, getWithExpiry } from './storage';
+import Parser from "rss-parser";
+import { Buffer } from "buffer";
+import { storeWithExpiry, getWithExpiry } from "./storage";
 
 const FEED_PARSER: Parser = new Parser();
 const CORS_PROXY = Buffer.from(
-  "aHR0cHM6Ly9ob21lcGFnZS5kZXZhbG9wZXMud29ya2Vycy5kZXYvY29yc3Byb3h5P3VybD0=", "base64"
-);
+  "aHR0cHM6Ly9hbG9wZXMuZGV2L2Nsb3VkZmxhcmUvY29yc3Byb3h5P3VybD0=",
+  "base64"
+).toString("utf-8");
 
 async function getContent(
   url: string,
@@ -16,13 +17,18 @@ async function getContent(
   const storedData = await getWithExpiry(url);
   if (cacheTime && storedData) {
     return storedData;
-  };
-  const response = await fetch(proxy ? CORS_PROXY + url : url);
+  }
+  const response = await fetch(proxy ? CORS_PROXY + url : url, {
+    headers: {
+      Authorization:
+        "Basic YWxvcGVzLmRldjpoY2VGcmUvMjZQeWorcjRpNHh6MnVMZ0RZQStWUzU0",
+    },
+  });
   const data = json ? await response.json() : await response.text();
   if (response.ok) {
     if (cacheTime) {
       await storeWithExpiry(url, data, cacheTime);
-  }   
+    }
     return data;
   } else {
     console.error(`${JSON.stringify(data)}`);
@@ -31,13 +37,12 @@ async function getContent(
 }
 
 export async function getRssFeed(url: string, cacheTime: number = 3_600_000) {
-    const text = await getContent(url, true, false, cacheTime);
-    try {
-        const feed =  await FEED_PARSER.parseString(text);
-        console.log(feed)
-        return feed
-    }
-    catch (exception){
-        console.error(`Failed to parse ${url}: ${exception}`)
-    }
+  const text = await getContent(url, true, false, cacheTime);
+  try {
+    const feed = await FEED_PARSER.parseString(text);
+    console.log(feed);
+    return feed;
+  } catch (exception) {
+    console.error(`Failed to parse ${url}: ${exception}`);
+  }
 }
