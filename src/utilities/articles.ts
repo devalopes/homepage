6da9b9b7c4f5eeb882df8c445444011251ffe479
Alex Lopes <alexjonathanlopes@gmail.com>
2022-12-31 15:10:38 -0500

    Remove proxy in favor of  build time rendering

--- src/utilities/articles.ts
+++ src/utilities/articles.ts
@@ -1,46 +1,23 @@
-import Parser from "rss-parser";
-import { Buffer } from "buffer";
-import { storeWithExpiry, getWithExpiry } from "./storage";
+import { parseFeed } from "htmlparser2";
 
-const FEED_PARSER: Parser = new Parser();
-const CORS_PROXY = Buffer.from(
-  "aHR0cHM6Ly9hbG9wZXMuZGV2L2Nsb3VkZmxhcmUvY29yc3Byb3h5P3VybD0=",
-  "base64"
-).toString("utf-8");
-
-async function getContent(
-  url: string,
-  proxy: boolean,
-  json: boolean,
-  cacheTime?: number
-) {
-  const storedData = await getWithExpiry(url);
-  if (cacheTime && storedData) {
-    return storedData;
-  }
-  const response = await fetch(proxy ? CORS_PROXY + url : url, {
-    headers: {
-      Authorization:
-        "Basic YWxvcGVzLmRldjpoY2VGcmUvMjZQeWorcjRpNHh6MnVMZ0RZQStWUzU0",
-    },
-  });
-  const data = json ? await response.json() : await response.text();
-  if (response.ok) {
-    if (cacheTime) {
-      await storeWithExpiry(url, data, cacheTime);
+async function getContent(url: string, json: boolean = false) {
+  try {
+    const response = await fetch(url);
+    const data = json ? await response.json() : await response.text();
+    if (response.ok) {
+      return data;
+    } else {
+      console.error(`Fetch response ${response.status}: ${JSON.stringify(data)}`);
     }
-    return data;
-  } else {
-    console.error(`${JSON.stringify(data)}`);
-    throw new Error(`Failed to load ${url}`);
+  } catch (exception) {
+    console.error(`Failed to get ${url}: ${exception}`);
   }
 }
 
-export async function getRssFeed(url: string, cacheTime: number = 3_600_000) {
-  const text = await getContent(url, true, false, cacheTime);
+export async function getRssFeed(url: string) {
+  const text = await getContent(url);
   try {
-    const feed = await FEED_PARSER.parseString(text);
-    console.log(feed);
+    const feed = parseFeed(text);
     return feed;
   } catch (exception) {
     console.error(`Failed to parse ${url}: ${exception}`);
